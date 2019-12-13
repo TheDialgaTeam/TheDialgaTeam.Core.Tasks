29a87fcca09a63d1896c2e7bf5905e77cc20fdce
Yong Jian Ming <jianming1993@gmail.com>
2019-12-13 20:47:01 +0800

    Added fire and forget functions.

--- src/TaskState.cs
+++ src/TaskState.cs
@@ -4,8 +4,10 @@ using System.Threading.Tasks;
 
 namespace TheDialgaTeam.Core.Tasks
 {
-    public class TaskState
+    public static class TaskState
     {
+        public static event UnhandledExceptionEventHandler UnhandledException;
+
         public static Task Run<TState>(TState state, Action<TState> action)
         {
             return Task.Factory.StartNew(action, state, default, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
@@ -24,6 +26,47 @@ namespace TheDialgaTeam.Core.Tasks
         public static Task<TResult> Run<TState, TResult>(TState state, Func<TState, TResult> function, CancellationToken cancellationToken)
         {
             return Task.Factory.StartNew(function, state, cancellationToken, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
+        }
+
+        public static void RunAndForget<TState>(TState state, Action<TState> action)
+        {
+            CatchUnhandledException(Run(state, action));
+        }
+
+        public static void RunAndForget<TState>(TState state, Action<TState> action, CancellationToken cancellationToken)
+        {
+            CatchUnhandledException(Run(state, action, cancellationToken), cancellationToken);
+        }
+
+        public static void RunAndForget<TState, TResult>(TState state, Func<TState, TResult> function)
+        {
+            CatchUnhandledException(Run(state, function));
+        }
+
+        public static void RunAndForget<TState, TResult>(TState state, Func<TState, TResult> function, CancellationToken cancellationToken)
+        {
+            CatchUnhandledException(Run(state, function, cancellationToken), cancellationToken);
+        }
+
+        private static void CatchUnhandledException(Task task, CancellationToken cancellationToken = default)
+        {
+            task.ContinueWith(innerTask =>
+            {
+                if (innerTask.Exception != null)
+                    UnhandledException?.Invoke(innerTask, new UnhandledExceptionEventArgs(innerTask.Exception, false));
+            }, cancellationToken);
+        }
+
+        private static void CatchUnhandledException<TResult>(Task<TResult> task, CancellationToken cancellationToken = default)
+        {
+            task.ContinueWith(innerTask =>
+            {
+                if (innerTask.Exception != null)
+                    UnhandledException?.Invoke(innerTask, new UnhandledExceptionEventArgs(innerTask.Exception, false));
+
+                if (innerTask.Result is Task innerResultTask)
+                    CatchUnhandledException(innerResultTask, cancellationToken);
+            }, cancellationToken);
         }
     }
 }
